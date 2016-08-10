a44c0e61c757718f110b071ac16fa04a84d78623
AtiLion <eliteinfinity1337@gmail.com>
2016-08-10 14:25:59 +0200

    Code
    Code

---
+++ FixLoading/Hook.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using SDG.Unturned;
+
+namespace FixLoading
+{
+    public class Hook
+    {
+        private static AppDomain mainDomain = AppDomain.CurrentDomain;
+
+        public static void hook()
+        {
+            Debug.Log("Hooking...");
+            try
+            {
+                mainDomain.AssemblyLoad += new AssemblyLoadEventHandler(onASMLoaded);
+                Debug.Log("Hook complete!");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("Failed to hook!");
+                Debug.LogException(ex);
+            }
+        }
+
+        private static void onASMLoaded(object sender, AssemblyLoadEventArgs args)
+        {
+            Debug.Log("Loaded: " + args.LoadedAssembly.FullName);
+            if (args.LoadedAssembly.FullName.ToLower().Contains("csharp") && !args.LoadedAssembly.FullName.ToLower().Contains("first"))
+            {
+                ASMExploit.jmp(typeof(Provider).GetMethod("send", BindingFlags.Public | BindingFlags.Static), typeof(Overridable).GetMethod("send", BindingFlags.Public | BindingFlags.Static));
+            }
+        }
+    }
+}
---
+++ FixLoading/Overridable.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using SDG.Unturned;
+using UnityEngine;
+using Steamworks;
+
+namespace FixLoading
+{
+    public class Overridable
+    {
+        private static int maxRetryAmount = 3;
+
+        private static void changeField(string name, object value)
+        {
+            typeof(Provider).GetField(name, BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, value);
+        }
+
+        private static object getField(string name)
+        {
+            return typeof(Provider).GetField(name, BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+        }
+
+        private static object runMethod(string name, object[] args)
+        {
+            return typeof(Provider).GetMethod(name, BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, args);
+        }
+
+        public static void send(CSteamID steamID, ESteamPacket type, byte[] packet, int size, int channel) // Override
+        {
+            if (!Provider.isConnected)
+            {
+                return;
+            }
+            changeField("_bytesSent", (uint)getField("_bytesSent") + (uint)size);
+            changeField("_packetsSent", (uint)getField("_packetsSent") + 1u);
+            if (Provider.isServer)
+            {
+                if (steamID == Provider.server || (Provider.isClient && steamID == Provider.client))
+                {
+                    runMethod("receiveServer", new object[]{
+                        Provider.server,
+                        packet,
+                        0,
+                        size,
+                        channel,
+                    });
+                    return;
+                }
+                if (steamID.m_SteamID == 0uL)
+                {
+                    Debug.LogError("Failed to send to invalid steam ID.");
+                    return;
+                }
+                if ((bool)runMethod("isUnreliable", new object[]{type}))
+                {
+                    if (!SteamGameServerNetworking.SendP2PPacket(steamID, packet, (uint)size, (!(bool)runMethod("isInstant", new object[] { type })) ? EP2PSend.k_EP2PSendUnreliable : EP2PSend.k_EP2PSendUnreliableNoDelay, channel))
+                    {
+                        Debug.LogError(string.Concat(new object[]
+				        {
+					        "Failed to send size ",
+					        size,
+					        " unreliable packet to ",
+					        steamID,
+					        "!"
+				        }));
+                    }
+                    return;
+                }
+                if (!SteamGameServerNetworking.SendP2PPacket(steamID, packet, (uint)size, (!(bool)runMethod("isInstant", new object[] { type })) ? EP2PSend.k_EP2PSendReliableWithBuffering : EP2PSend.k_EP2PSendReliable, channel))
+                {
+                    Debug.LogError(string.Concat(new object[]
+			        {
+				        "Failed to send size ",
+				        size,
+				        " reliable packet to ",
+				        steamID,
+				        "!"
+			        }));
+                }
+            }
+            else
+            {
+                if (steamID == Provider.client)
+                {
+                    runMethod("receiveClient", new object[]{
+                        Provider.client,
+                        packet,
+                        0,
+                        size,
+                        channel,
+                    });
+                    Provider._connectionFailureInfo = ESteamConnectionFailureInfo.LATE_PENDING;
+                    Provider.disconnect();
+                    return;
+                }
+                if (steamID.m_SteamID == 0uL)
+                {
+                    Debug.LogError("Failed to send to invalid steam ID.");
+                    Provider._connectionFailureInfo = ESteamConnectionFailureInfo.LATE_PENDING;
+                    Provider.disconnect();
+                    return;
+                }
+                if ((bool)runMethod("isUnreliable", new object[] { type }))
+                {
+                    bool suc = false;
+                    for (int i = 0; i < maxRetryAmount; i++)
+                    {
+                        if (!SteamNetworking.SendP2PPacket(steamID, packet, (uint)size, (!(bool)runMethod("isInstant", new object[] { type })) ? EP2PSend.k_EP2PSendUnreliable : EP2PSend.k_EP2PSendUnreliableNoDelay, channel))
+                        {
+                            Debug.LogError(string.Concat(new object[]
+				            {
+					            "Failed to send size ",
+					            size,
+					            " unreliable packet to ",
+					            steamID,
+					            "!"
+				            }));
+                        }
+                        else
+                        {
+                            suc = true;
+                            break;
+                        }
+                    }
+                    if (!suc)
+                    {
+                        Provider._connectionFailureInfo = ESteamConnectionFailureInfo.LATE_PENDING;
+                        Provider.disconnect();
+                    }
+                    return;
+                }
+                bool suc1 = false;
+                for (int i = 0; i < maxRetryAmount; i++)
+                {
+                    if (!SteamNetworking.SendP2PPacket(steamID, packet, (uint)size, (!(bool)runMethod("isInstant", new object[] { type })) ? EP2PSend.k_EP2PSendReliableWithBuffering : EP2PSend.k_EP2PSendReliable, channel))
+                    {
+                        Debug.LogError(string.Concat(new object[]
+			            {
+				            "Failed to send size ",
+				            size,
+				            " reliable packet to ",
+				            steamID,
+				            "!"
+			            }));
+                    }
+                    else
+                    {
+                        suc1 = true;
+                        break;
+                    }
+                    if (!suc1)
+                    {
+                        Provider._connectionFailureInfo = ESteamConnectionFailureInfo.LATE_PENDING;
+                        Provider.disconnect();
+                    }
+                }
+            }
+        }
+    }
+}
